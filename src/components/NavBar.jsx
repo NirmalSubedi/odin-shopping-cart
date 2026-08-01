ee65fb837d3cf7619d7ec0454b93c4164bb0d721
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2026-07-31 19:08:44 -0700

    feat: add NavBar component
    To traverse across the app pages.

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,32 @@
+import { NavLink } from "react-router";
+import { CartPreview } from "./index.jsx";
+
+export const NavBar = ({ cart }) => {
+  const styleNavLink = ({ isActive }) => ({
+    color: isActive ? "red" : "",
+  });
+
+  return (
+    <nav>
+      <ul
+        style={{ display: "flex", gap: "1em", listStyle: "none", padding: 0 }}
+      >
+        <li>
+          <NavLink to="/" style={styleNavLink}>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/shop" style={styleNavLink}>
+            Shop
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/cart" style={styleNavLink}>
+            <CartPreview {...{ cart }} />
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
