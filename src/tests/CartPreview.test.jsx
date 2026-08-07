916bdf69f2fbc35bd2d5f3e980e17978eb9ab152
Nirmal Subedi <101914003+NirmalSubedi@users.noreply.github.com>
2026-08-06 19:18:40 -0700

    test(CartPreview): add test cases for values

---
+++ src/tests/CartPreview.test.jsx
@@ -0,0 +1,25 @@
+import { render, screen } from "@testing-library/react";
+import { expect, it } from "vitest";
+import { CartPreview } from "../components";
+
+it("shows cart with '0' value when empty", () => {
+  render(<CartPreview />);
+
+  expect(screen.getByText("Cart (0)")).toBeInTheDocument();
+});
+
+it("shows cart with value 1 for one item in cart", () => {
+  const products = [{ quantity: 1 }];
+
+  render(<CartPreview {...{ products }} />);
+
+  expect(screen.getByText(/1/)).toBeInTheDocument();
+});
+
+it("shows cart with value that is the sum of all product quantities", () => {
+  const products = [{ quantity: 1 }, { quantity: 2 }, { quantity: 3 }];
+
+  render(<CartPreview {...{ products }} />);
+
+  expect(screen.getByText(/6/)).toBeInTheDocument();
+});
